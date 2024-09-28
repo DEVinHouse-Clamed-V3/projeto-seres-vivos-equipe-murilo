2e105653476a73fa0af3029aa6ed28db78964a56
p-skipper <kipperspedro@gmail.com>
2024-09-28 15:48:44 -0300

    consome api plantas

--- src/pages/Plantas.js
+++ src/pages/Plantas.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
-import { globalStyles } from '../styles/global';
+import React, { useEffect, useState } from "react";
+import { Text, SafeAreaView, FlatList } from "react-native";
+import { globalStyles } from "../styles/global";
+import axios from "axios";
 
-const Plantas  = () => {
+export default function Plantae() {
+  const [plantas, setPlantas] = useState([]);
+
+  async function carregarDados() {
+    try {
+      const response = await axios.get("http://192.168.0.23:3000/plantas");
+      setPlantas(response.data);
+    } catch (error) {
+      Alert.alert("Erro ao obter dados:", error);
+    }
+  }
+  useEffect(() => {
+    carregarDados();
+  }, []);
 
   return (
     <SafeAreaView style={globalStyles.container}>
-      <Text>Tela do reino plantas</Text>
+      <FlatList />
     </SafeAreaView>
   );
-};
-
-
-export default Plantas;
+}
